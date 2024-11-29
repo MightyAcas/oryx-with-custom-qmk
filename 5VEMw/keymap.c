b70ddd9b24f1c83e24851fffd4853cfda13db3dc
Acas <67846394+MightyAcas@users.noreply.github.com>
2024-11-29 18:55:49 -0500

    Update keymap.c

--- 5VEMw/keymap.c
+++ 5VEMw/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [1] = LAYOUT_voyager(
     KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,                                          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,         
-    KC_TRANSPARENT, TO(3),          KC_MS_WH_UP,    KC_MS_UP,       KC_MS_WH_DOWN,  LED_LEVEL,                                      KC_ASTR,        KC_7,           KC_8,           KC_9,           KC_MINUS,       KC_TRANSPARENT, 
+    KC_TRANSPARENT, TO(3),          KC_MS_WH_UP,    KC_MS_UP,       KC_MS_WH_DOWN,  RGB_MODE_FORWARD,                               KC_ASTR,        KC_7,           KC_8,           KC_9,           KC_MINUS,       KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_MS_BTN1,     KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_BTN2,                                     KC_SLASH,       KC_4,           KC_5,           KC_6,           KC_PLUS,        KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_MS_WH_LEFT,  KC_PSCR,        KC_MS_BTN3,     QK_LLCK,        KC_MS_WH_RIGHT,                                 KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_TRANSPARENT, KC_TRANSPARENT, 
                                                     KC_TRANSPARENT, TO(0),                                          LCTL(KC_BSPC),  KC_0
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, RGB_VAI,        KC_HOME,        KC_UP,          KC_END,         KC_PAGE_UP,                                     KC_PIPE,        KC_AMPR,        KC_QUES,        KC_EXLM,        KC_TILD,        KC_TRANSPARENT, 
     KC_TRANSPARENT, RGB_VAD,        KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_PGDN,                                        KC_BSLS,        KC_DLR,         KC_PERC,        KC_CIRC,        KC_GRAVE,       KC_TRANSPARENT, 
-    KC_TRANSPARENT, KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          T_S_C,          QK_LLCK,        KC_AT,          KC_HASH,        KC_NO,          KC_TRANSPARENT, 
+    KC_TRANSPARENT, RGB_SPD,        RGB_SPI,        KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          QK_LLCK,        KC_AT,          KC_HASH,        KC_NO,          KC_TRANSPARENT, 
                                                     KC_LEFT_GUI,    OSM(MOD_LALT),                                  TO(0),          KC_NO
   ),
   [3] = LAYOUT_voyager(
@@ -49,60 +49,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 
-extern rgb_config_t rgb_matrix_config;
-
-void keyboard_post_init_user(void) {
-  rgb_matrix_enable();
-}
-
-const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
-    [0] = { {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116} },
-
-    [1] = { {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,245,245}, {193,230,116}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116} },
-
-    [2] = { {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {193,230,116}, {193,230,116}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {193,230,116}, {193,230,116}, {193,230,116}, {193,230,116}, {0,0,0}, {0,0,0}, {193,230,116}, {74,255,255} },
-
-};
-
-void set_layer_color(int layer) {
-  for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
-    HSV hsv = {
-      .h = pgm_read_byte(&ledmap[layer][i][0]),
-      .s = pgm_read_byte(&ledmap[layer][i][1]),
-      .v = pgm_read_byte(&ledmap[layer][i][2]),
-    };
-    if (!hsv.h && !hsv.s && !hsv.v) {
-        rgb_matrix_set_color( i, 0, 0, 0 );
-    } else {
-        RGB rgb = hsv_to_rgb( hsv );
-        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
-        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
-    }
-  }
-}
-
-bool rgb_matrix_indicators_user(void) {
-  if (rawhid_state.rgb_control) {
-      return false;
-  }
-  if (keyboard_config.disable_layer_led) { return false; }
-  switch (biton32(layer_state)) {
-    case 0:
-      set_layer_color(0);
-      break;
-    case 1:
-      set_layer_color(1);
-      break;
-    case 2:
-      set_layer_color(2);
-      break;
-   default:
-    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
-      rgb_matrix_set_color_all(0, 0, 0);
-    break;
-  }
-  return true;
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (!process_sentence_case(keycode, record)) { return false; }
