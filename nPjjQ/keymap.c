3d0cb72eda194eb5e1b0cfdb6486b38496596529
Acas <67846394+MightyAcas@users.noreply.github.com>
2026-04-30 17:32:39 -0400

    Update keymap.c

--- nPjjQ/keymap.c
+++ nPjjQ/keymap.c
@@ -13,7 +13,6 @@ uint16_t last_mod_manual = 0; // for timer reset and adaptive keys
 enum custom_keycodes {
   RGB_SLD = ZSA_SAFE_RANGE,
   MAGIC_KEY,
-  REP_OSM,
 };
 
 
@@ -25,14 +24,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_W,           KC_C,           KC_M,           KC_P,           KC_K,                                           KC_Z,           KC_L,           KC_O,           KC_U,           KC_J,           KC_MINUS,       
     KC_BSPC,        KC_R,           KC_S,           KC_T,           KC_H,           KC_F,                                           KC_X,           KC_N,           KC_A,           KC_E,           KC_I,           KC_ENTER,       
     LCTL(KC_Z),     KC_Q,           KC_V,           KC_G,           KC_D,           KC_B,                                           KC_SCLN,        MAGIC_KEY,      KC_COMMA,       KC_QUOTE,       KC_DOT,         KC_ESCAPE,      
-                                                    KC_SPACE,       OSL(1),                                         REP_OSM,  KC_Y
+                                                    KC_SPACE,       OSL(1),                                         OSM(MOD_RSFT),  KC_Y
   ),
   [1] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_F2,          KC_PSCR,        KC_HOME,        KC_UP,          KC_END,         KC_PAGE_UP,                                     KC_BSLS,        KC_7,           KC_8,           KC_9,           LED_LEVEL,      KC_F5,          
     KC_TRANSPARENT, QK_LLCK,        KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_PGDN,                                        KC_GRAVE,       KC_4,           KC_5,           KC_6,           KC_EQUAL,       KC_F8,          
     KC_F4,          KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_UP,       KC_MS_RIGHT,    KC_MS_BTN2,                                     KC_SLASH,       KC_1,           KC_2,           KC_3,           KC_TRANSPARENT, KC_F11,         
-                                                    KC_MS_BTN1,     TO(0),                                          OSM(MOD_RSFT), KC_0
+                                                    KC_MS_BTN1,     TO(0),                                          KC_TRANSPARENT, KC_0
   ),
   [2] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
@@ -105,7 +104,6 @@ bool remember_last_key_user(uint16_t keycode, keyrecord_t* record,
                             uint8_t* remembered_mods) {
     switch (keycode) {
         case MAGIC_KEY:
-        case REP_OSM:
             return false;  // Ignore ALTREP keys. If this is not here, only default action will be done, since the process stuff is apparently run after the remember last key stuff, so that the key is only ever trying to repeat itself.
     }
 
@@ -246,20 +244,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           if (alpha_pressed) {// letter was pressed within timer limits
             arcane_timer = timer_read(); // reset timer
             process_arcane_l(get_last_keycode(), get_last_mods()); // call arcane code
-          } else {//alpha timer timed out, so key functions just as a OSM shift
-            set_oneshot_mods(MOD_BIT(KC_LSFT));
-          }
-        }
-      }
-      break;
-    case REP_OSM: 
-      if (record->event.pressed && layer_state_is(0)) {
-        if (get_oneshot_mods() & MOD_MASK_SHIFT) {
-          caps_word_toggle(); //toggles on caps word if one shot shift is already active (e.g. through double-tapping the key
-        } else {
-          if (alpha_pressed) {// letter was pressed within timer limits
-            arcane_timer = timer_read(); // reset timer
-            repeat_key_invoke(&record->event);
           } else {//alpha timer timed out, so key functions just as a OSM shift
             set_oneshot_mods(MOD_BIT(KC_LSFT));
           }
