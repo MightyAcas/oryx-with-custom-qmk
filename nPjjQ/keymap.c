e3ed091bd9c0900b0afed1899ca966c2999a6bde
Acas <67846394+MightyAcas@users.noreply.github.com>
2026-05-05 02:45:24 -0400

    Update keymap.c

--- nPjjQ/keymap.c
+++ nPjjQ/keymap.c
@@ -5,8 +5,14 @@
 #define ZSA_SAFE_RANGE SAFE_RANGE
 #endif
 
+bool alpha_pressed = false; // variable for timer to disable arcane key functionality after no letter has been pressed for x amount of time
+uint16_t arcane_timer = 0;     // timer 
+uint16_t last_key_manual = 0; // for timer reset and adaptive keys
+uint16_t last_mod_manual = 0; // for timer reset and adaptive keys
+
 enum custom_keycodes {
   RGB_SLD = ZSA_SAFE_RANGE,
+  REP_OSM,
 };
 
 
@@ -18,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_C,           KC_M,           KC_P,           KC_K,                                           KC_J,           KC_Y,           KC_O,           KC_U,           KC_B,           KC_MINUS,       
     KC_BSPC,        KC_R,           KC_S,           KC_T,           KC_H,           KC_F,                                           KC_X,           KC_N,           KC_A,           KC_E,           KC_I,           KC_ENTER,       
     LCTL(KC_Z),     KC_L,           KC_G,           KC_V,           KC_D,           KC_W,                                           KC_Z,           KC_COMMA,       KC_QUOTE,       KC_SCLN,        KC_DOT,         KC_ESCAPE,      
-                                                    KC_SPACE,       OSL(1),                                         RCTL(KC_BSPC),  OSM(MOD_RSFT)
+                                                    KC_SPACE,       OSL(1),                                         RCTL(KC_BSPC),  REP_OSM
   ),
   [1] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
@@ -95,13 +101,64 @@ combo_t key_combos[COMBO_COUNT] = {
 };
 
 
+bool remember_last_key_user(uint16_t keycode, keyrecord_t* record,
+                            uint8_t* remembered_mods) {
+    switch (keycode) {
+        case REP_OSM:
+            return false;  // Ignore ALTREP keys. If this is not here, only default action will be done, since the process stuff is apparently run after the remember last key stuff, so that the key is only ever trying to repeat itself.
+    }
 
+    return true;  // Other keys can be repeated.
+}
 
+void matrix_scan_user(void) { // The very important timer.
+  if (alpha_pressed && timer_elapsed(arcane_timer) > 1000) { //triggers when timer elapsed
+      alpha_pressed = false;
+      set_last_keycode(KC_SPACE);
+  } else { //timer update
+    switch (get_last_keycode()) {
+      case KC_A ... KC_Z:
+      case KC_SCLN:
+      case KC_COMMA:
+      case KC_DOT:
+        if (last_key_manual != get_last_keycode()) {
+          last_key_manual = get_last_keycode();
+          last_mod_manual = get_last_mods();
+          alpha_pressed = true;
+          arcane_timer = timer_read();
+        }
+      break; //these were all the keys that keep the timer going
+      case KC_SPACE:
+      case KC_ENTER:
+      case KC_BSPC:
+      case RCTL(KC_BSPC):
+      last_key_manual = get_last_keycode();
+      alpha_pressed = false;
+      break; //these were all the keys that end the timer prematurely
+    }
+  }
+}
 
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    case REP_OSM: 
+      if (record->event.pressed && layer_state_is(0)) {
+        if (get_oneshot_mods() & MOD_MASK_SHIFT) {
+          caps_word_toggle(); //toggles on caps word if one shot shift is already active (e.g. through double-tapping the key
+        } else {
+          if (alpha_pressed) {// letter was pressed within timer limits
+            arcane_timer = timer_read(); // reset timer
+            repeat_key_invoke(&record->event);
+          } else {//alpha timer timed out, so key functions just as a OSM shift
+            set_oneshot_mods(MOD_BIT(KC_LSFT));
+          }
+        }
+      }
+      break;
+
+
   case QK_MODS ... QK_MODS_MAX:
     // Mouse and consumer keys (volume, media) with modifiers work inconsistently across operating systems,
     // this makes sure that modifiers are always applied to the key that was pressed.
