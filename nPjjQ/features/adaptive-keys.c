4b06f2147ed075b95b41104b865613e171705bda
Acas <67846394+MightyAcas@users.noreply.github.com>
2026-05-14 00:46:55 -0400

    Update adaptive-keys.c

--- nPjjQ/features/adaptive-keys.c
+++ nPjjQ/features/adaptive-keys.c
@@ -1,5 +1,5 @@
 /*
-  Copyright 2018-2022 Eric Gebhart <e.a.gebhart@gmail.com>
+  Copyright 2018-2022 EricaLinaQi <ericalinaqi@proton.me>>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
