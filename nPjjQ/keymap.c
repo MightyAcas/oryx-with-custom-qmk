b4c3f857f3578bb2ee32a56fe5e289dc6451c2d0
Acas <67846394+MightyAcas@users.noreply.github.com>
2026-06-10 11:16:11 -0400

    Update keymap.c

--- nPjjQ/keymap.c
+++ nPjjQ/keymap.c
@@ -16,9 +16,9 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
-    KC_TAB,         KC_W,           KC_C,           KC_M,           KC_P,           KC_K,                                           KC_X,           KC_Y,           KC_O,           KC_U,           KC_J,           KC_MINUS,       
+    KC_TAB,         KC_W,           KC_C,           KC_M,           KC_P,           KC_K,                                           KC_X,           KC_L,           KC_O,           KC_U,           KC_J,           KC_MINUS,       
     KC_BSPC,        KC_R,           KC_S,           KC_T,           KC_H,           KC_F,                                           KC_QUOTE,       KC_N,           KC_A,           KC_E,           KC_I,           KC_ENTER,       
-    LCTL(KC_BSPC),  KC_Q,           KC_G,           KC_V,           KC_D,           KC_B,                                           KC_Z,           KC_L,           KC_DOT,         KC_SCLN,        KC_COMMA,       OSM(MOD_LALT),  
+    LCTL(KC_BSPC),  KC_Q,           KC_G,           KC_V,           KC_D,           KC_B,                                           KC_Z,           KC_Y,           KC_DOT,         KC_SCLN,        KC_COMMA,       OSM(MOD_LALT),  
                                                     KC_SPACE,       OSL(1),                                         OSL(2),         OSM(MOD_RSFT)
   ),
   [1] = LAYOUT_voyager(
@@ -55,7 +55,7 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
 const uint16_t PROGMEM combo0[] = { KC_DELETE, KC_SPACE, COMBO_END};
 const uint16_t PROGMEM combo1[] = { KC_TAB, KC_Q, COMBO_END};
 const uint16_t PROGMEM combo2[] = { KC_P, KC_H, COMBO_END};
-const uint16_t PROGMEM combo3[] = { KC_Y, KC_N, COMBO_END};
+const uint16_t PROGMEM combo3[] = { KC_L, KC_N, COMBO_END};
 const uint16_t PROGMEM combo4[] = { KC_M, KC_T, COMBO_END};
 const uint16_t PROGMEM combo5[] = { KC_O, KC_A, COMBO_END};
 const uint16_t PROGMEM combo6[] = { KC_C, KC_S, COMBO_END};
